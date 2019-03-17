d75d631350d32522bc4827c1ff3f90e6bd2cfa24
zuisong <378818573@qq.com>
2019-03-17 17:37:21 +0800

    [51] N皇后

--- examples/q51.rs
+++ examples/q51.rs
@@ -1,8 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=rust
+ *
+ * [51] N皇后
+ *
+ * https://leetcode-cn.com/problems/n-queens/description/
+ *
+ * algorithms
+ * Hard (58.96%)
+ * Total Accepted:    4.8K
+ * Total Submissions: 8.1K
+ * Testcase Example:  '4'
+ *
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ *
+ *
+ * 上图为 8 皇后问题的一种解法。
+ *
+ * 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+ *
+ * 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ *
+ * 示例:
+ *
+ * 输入: 4
+ * 输出: [
+ * ⁠[".Q..",  // 解法 1
+ * ⁠ "...Q",
+ * ⁠ "Q...",
+ * ⁠ "..Q."],
+ *
+ * ⁠["..Q.",  // 解法 2
+ * ⁠ "Q...",
+ * ⁠ "...Q",
+ * ⁠ ".Q.."]
+ * ]
+ * 解释: 4 皇后问题存在两个不同的解法。
+ *
+ *
+ */
+
 extern crate log;
 extern crate simple_logger;
 
 use log::*;
-use std::process::exit;
 
 struct Solution {}
 
@@ -11,15 +52,13 @@ const QUEEN_GRID: &str = "Q ";
 
 impl Solution {
     pub fn dfs(v: &mut Vec<i32>, layer: i32, result: &mut Vec<Vec<String>>) {
-        //        if result.len() > 0 {
+        //        if result.len() > 0 {  // 找到一个就结束
         //            return;
         //        }
-        info!("{:?}", v);
+//        info!("{:?}", v);
 
         let n = v.len() as i32;
         if layer == n {
-            //                        println!("{:?}", v);
-
             let res: Vec<String> = v
                 .iter()
                 .map(|it| {
@@ -27,13 +66,12 @@ impl Solution {
                     s[*it as usize] = QUEEN_GRID;
 
                     let s = s.join("");
-                    info!("{}", s);
+//                    info!("{}", s);
                     s
                 })
                 .collect();
 
             result.push(res);
-            exit(0);
 
             return;
         }
@@ -64,13 +102,12 @@ impl Solution {
 
 fn main() {
     simple_logger::init().unwrap();
-    let result = Solution::solve_n_queens(31);
+    let result = Solution::solve_n_queens(8);
 
     info!("{}", result.len());
 
     for (idx, res) in result.iter().enumerate() {
         info!("===== {}", idx + 1);
-
         for r in res {
             info!("{}", r);
         }

80779b504619f11327233c99259fcb154fa1b3c2
zuisong <378818573@qq.com>
2019-03-26 22:16:49 +0800

    [88] 合并两个有序数组

---
+++ examples/q88.rs
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=rust
+ *
+ * [88] 合并两个有序数组
+ *
+ * https://leetcode-cn.com/problems/merge-sorted-array/description/
+ *
+ * algorithms
+ * Easy (43.11%)
+ * Total Accepted:    35.1K
+ * Total Submissions: 81.1K
+ * Testcase Example:  '[1,2,3,0,0,0]\n3\n[2,5,6]\n3'
+ *
+ * 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+ *
+ * 说明:
+ *
+ *
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ *
+ *
+ * 示例:
+ *
+ * 输入:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ *
+ * 输出: [1,2,2,3,5,6]
+ *
+ */
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let mut m = m as usize;
+        let mut n = n as usize;
+
+        while n != 0 {
+            if m == 0 || nums2[n - 1] >= nums1[m - 1] {
+                nums1[m + n - 1] = nums2[n - 1];
+                n -= 1;
+            } else {
+                nums1[m + n - 1] = nums1[m - 1];
+                m -= 1;
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut nums1 = vec![1, 2, 3, 0, 0, 0];
+    let mut nums2 = vec![2, 5, 6];
+    let m = 3;
+    let n = 3;
+
+
+    Solution::merge(&mut nums1, m, &mut nums2, n);
+    assert_eq!(nums1, vec![1, 2, 2, 3, 5, 6]);
+}
+
+struct Solution {}

38d6d91c5815dcc54b3f1d51cafc5b0a72c3ad99
zuisong <378818573@qq.com>
2019-06-13 22:37:58 +0800

    [137] 只出现一次的数字 II   补全注释

--- examples/q137.rs
+++ examples/q137.rs
@@ -21,12 +21,16 @@
 
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> i32 {
-        let mut one = 0;
-        let mut two = 0;
+        let mut one = 0; // 某位为1表示该位出现了1次
+        let mut two = 0; // 某位为1表示该位出现了2次
         nums.into_iter().for_each(|num| {
+            //  one & num   上一轮的one 和 num 同时为1  &后为1的位则表示 哪些位需要从1位变为2位
             two |= one & num;
+            // 新一轮的one 应该是 上一轮的one 和 num 不同时为 1 的位，因为同时为1的位已经被上一行统计过了
             one ^= num;
+            // 如果  one 和 two 同时为1 则表示该位出现三次了
             let three = one & two;
+            // 将出现了三次的位置0
             one &= !three;
             two &= !three;
         });

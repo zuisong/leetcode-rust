a072f3489a917ae7596c31ab17b001029b6a15b6
chenjian <com.me@foxmail.com>
2020-12-29 16:42:02 +0800

    q3 不含有重复字符的 最长子串 的长度

---
+++ src/bin/q3.rs
@@ -0,0 +1,109 @@
+/*
+
+
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+
+
+示例 1:
+
+输入: s = "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+示例 4:
+
+输入: s = ""
+输出: 0
+
+
+提示：
+
+0 <= s.length <= 5 * 104
+s 由英文字母、数字、符号和空格组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+*/
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let s: Vec<_> = s.chars().collect();
+
+        let mut set = HashSet::new();
+        let mut ans = 0;
+        let mut i = 0;
+        let mut ridx = -1;
+        while i < s.len() {
+            if i != 0 {
+                set.remove(&s[i - 1]);
+            }
+
+            while ridx + 1 < s.len() as i32 && !set.contains(&s[(ridx + 1) as usize]) {
+                set.insert(s[(ridx + 1) as usize]);
+                ridx += 1;
+            }
+
+            ans = ans.max(ridx - i as i32 + 1);
+            i += 1
+        }
+        ans
+    }
+}
+
+struct Solution {}
+
+fn main() {
+    let res = Solution::length_of_longest_substring("pwwkew".to_string());
+
+    println!("{}", res)
+}
+
+/*
+
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+*/

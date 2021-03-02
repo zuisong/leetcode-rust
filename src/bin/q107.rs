738308317e2c09a82066e89e9bcc423f1a67d051
chenjian <com.me@foxmail.com>
2021-03-03 00:27:40 +0800

    q107. 给定一个二叉树，返回其节点值自底向上的层序遍历。

---
+++ src/bin/q107.rs
@@ -0,0 +1,99 @@
+/*
+给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+
+例如：
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其自底向上的层序遍历为：
+
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+通过次数126,290提交次数185,242
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+//--
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn level_order_bottom(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        let mut result = vec![];
+        Self::helper(&root, 0, &mut result);
+        result.reverse();
+        return result;
+    }
+    fn helper(node: &Option<Rc<RefCell<TreeNode>>>, level: usize, result: &mut Vec<Vec<i32>>) {
+        match node {
+            None => {}
+            Some(node) => {
+                let n: &TreeNode = &node.borrow();
+                if level < result.len() {
+                    result[level].push(n.val);
+                } else {
+                    result.push(vec![n.val])
+                }
+                Self::helper(&n.left, level + 1, result);
+                Self::helper(&n.right, level + 1, result);
+            }
+        }
+    }
+}
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+struct Solution {}
+
+fn main() {
+    let tree: Option<Rc<RefCell<TreeNode>>> = Some(Rc::new(RefCell::new(TreeNode {
+        val: 3,
+        left: Some(Rc::new(RefCell::new(TreeNode {
+            val: 9,
+            left: None,
+            right: None,
+        }))),
+        right: Some(Rc::new(RefCell::new(TreeNode {
+            val: 20,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 15,
+                left: None,
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 7,
+                left: None,
+                right: None,
+            }))),
+        }))),
+    })));
+    let vec = Solution::level_order_bottom(tree);
+    dbg!(&vec);
+}
